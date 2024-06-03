3a12be38ef4bd0cd403093f58e636c2fdfa5358e
SakethSampathVinay <142145899+SakethSampathVinay@users.noreply.github.com>
2024-06-03 12:15:16 +0530

    Update functionality.js

--- functionality.js
+++ functionality.js
@@ -1,75 +1,307 @@
-const socialIconButtons = document.getElementsByClassName("social-icon");
+let todoUserInputEl = document.getElementById("todoUserInput");
+let todoItemsContainerEl = document.getElementById("todoItemsContainer");
+let addTodoButtonEl = document.getElementById("addTodoButton");
+let saveTodoButtonEl = document.getElementById("saveTodoButton");
+let tasksCompletedEl = document.getElementById("tasksCompleted");
+let tasksPendingEl = document.getElementById("tasksPending");
+let totalTasksEl = document.getElementById("totalTasks");
+let priorityIdEl = document.getElementById("priorityId");
+let myTasksIdEl = document.getElementById("myTasksId");
 
-function postToSocialMedia(classList) {
-    const content = `Check out my cool generative AI Project 😎 \n${window.location.href.toString()}`;
-    if (classList.contains("twitter")) {
-        const url = `https://twitter.com/intent/tweet?text=${encodeURIComponent(
-      content
-    )}`;
-        window.open(url);
-    } else if (classList.contains("linkedin")) {
-        const url = `https://www.linkedin.com/share?text=${encodeURIComponent(
-      content
-    )}`;
-        window.open(url);
-    } else if (classList.contains("whatsapp")) {
-        window.open(`https://api.whatsapp.com/send?text=${encodeURIComponent(content)}`);
+function getTodoListFromLocalStorage() {
+    let stringifiedTodoList = localStorage.getItem("todoList");
+    let parsedTodoList = JSON.parse(stringifiedTodoList);
+    if (parsedTodoList === null) {
+        return [];
+    } else {
+        return parsedTodoList;
+    }
+}
+let todoList = getTodoListFromLocalStorage();
+let todosCount = todoList.length;
+
+function showingCongratsMessage() {
+    let congratsMessagePopupContainer = document.createElement("div");
+    congratsMessagePopupContainer.classList.add("congrats-popup-5-seconds");
+    let congratsMessagePopupEl = document.createElement("h1");
+    congratsMessagePopupEl.classList.add("congrats-heading-popup");
+    congratsMessagePopupEl.textContent = "Congrats! You Have Completed all Your Tasks. Have a Great Day.";
+    congratsMessagePopupContainer.appendChild(congratsMessagePopupEl);
+    document.body.appendChild(congratsMessagePopupContainer);
+    todoItemsContainerEl.style.display = "none";
+    myTasksIdEl.style.display = "none";
+    saveTodoButtonEl.style.display = "none";
+    setTimeout(function() {
+        document.body.removeChild(congratsMessagePopupContainer);
+        myTasksIdEl.style.display = "";
+        todoItemsContainerEl.style.display = "";
+        saveTodoButtonEl.style.display = "";
+    }, 2000);
+}
+
+function showingPendingMessage() {
+    let pendingMessagePopupContainer = document.createElement("div");
+    pendingMessagePopupContainer.classList.add("congrats-popup-5-seconds");
+    let pendingMessagePopup = document.createElement("h1");
+    pendingMessagePopup.classList.add("congrats-heading-popup");
+    pendingMessagePopup.textContent = "Complete your Pending Tasks well. Have a Great Day!";
+    pendingMessagePopupContainer.appendChild(pendingMessagePopup);
+    document.body.appendChild(pendingMessagePopupContainer);
+    todoItemsContainerEl.style.display = "none";
+    myTasksIdEl.style.display = "none";
+    saveTodoButtonEl.style.display = "none";
+    setTimeout(function() {
+        document.body.removeChild(pendingMessagePopupContainer);
+        myTasksIdEl.style.display = "";
+        todoItemsContainerEl.style.display = "";
+        saveTodoButtonEl.style.display = "";
+    }, 1000);
+}
+
+function noTasksPopupMessage() {
+    let noTasksPopupMessageContainer = document.createElement("div");
+    noTasksPopupMessageContainer.classList.add("congrats-popup-5-seconds");
+    let noTasksMessagePopupEl = document.createElement("h1");
+    noTasksMessagePopupEl.classList.add("congrats-heading-popup");
+    noTasksMessagePopupEl.textContent = "Your to-do list is currently empty. Take a moment to plan out your day now.";
+    noTasksPopupMessageContainer.appendChild(noTasksMessagePopupEl);
+    document.body.appendChild(noTasksPopupMessageContainer);
+    todoItemsContainerEl.style.display = "none";
+    myTasksIdEl.style.display = "none";
+    saveTodoButtonEl.style.display = "none";
+    setTimeout(function() {
+        document.body.removeChild(noTasksPopupMessageContainer);
+        myTasksIdEl.style.display = "";
+        todoItemsContainerEl.style.display = "";
+        saveTodoButtonEl.style.display = "";
+    }, 2000);
+}
+
+function noTodolist() {
+    let noTodolistContainer = document.createElement("div");
+    noTodolistContainer.classList.add("congrats-popup-5-seconds");
+    let noTodolistEl = document.createElement("h1");
+    noTodolistEl.classList.add("congrats-heading-popup");
+    noTodolistEl.textContent = "Nothing to Save in TodoList. Plan out your Day to get things done.";
+    noTodolistContainer.appendChild(noTodolistEl);
+    document.body.appendChild(noTodolistContainer);
+    todoItemsContainerEl.style.display = "none";
+    myTasksIdEl.style.display = "none";
+    saveTodoButtonEl.style.display = "none";
+    setTimeout(function() {
+        document.body.removeChild(noTodolistContainer);
+        myTasksIdEl.style.display = "";
+        todoItemsContainerEl.style.display = "";
+        saveTodoButtonEl.style.display = "";
+    }, 2000);
+}
+
+if (todoList.length === 0) {
+    totalTasksEl.textContent = 0;
+    tasksCompletedEl.textContent = 0;
+    tasksPendingEl.textContent = 0;
+    noTasksPopupMessage();
+}
+let congratsMessageVisible = false;
+let pendingMessageVisible = false;
+
+function updateTasksCount() {
+    let completedTasks = 0;
+    for (let todo of todoList) {
+        if (todo.isChecked === true) {
+            completedTasks = completedTasks + 1;
+        }
+    }
+    totalTasksEl.textContent = todosCount;
+    let pendingTasks = todosCount - completedTasks;
+    tasksCompletedEl.textContent = completedTasks;
+    tasksPendingEl.textContent = pendingTasks;
+
+    if (completedTasks === todosCount && !congratsMessageVisible) {
+        showingCongratsMessage();
+        congratsMessageVisible = true;
+    } else if (completedTasks !== todosCount) {
+        congratsMessageVisible = false;
+    }
+
+    if (completedTasks !== todosCount && !pendingMessageVisible) {
+        showingPendingMessage();
+        pendingMessageVisible = true;
+    } else if (completedTasks !== todosCount) {
+        congratsMessageVisible = true;
     }
 }
 
-const addEventListenersToSocialIconButton = () => {
-    for (let i = 0; i < socialIconButtons.length; i++) {
-        socialIconButtons[i].addEventListener("click", () =>
-            postToSocialMedia(socialIconButtons[i].classList)
-        );
-    }
+
+document.addEventListener("DOMContentLoaded", function() {
+    saveTodoButtonEl.onclick = function() {
+        localStorage.setItem("todoList", JSON.stringify(todoList));
+        updateTasksCount();
+        location.reload(); // Reload the page
+    };
+});
+
+saveTodoButtonEl.onclick = function() {
+    localStorage.setItem("todoList", JSON.stringify(todoList));
+    updateTasksCount();
 };
 
-addEventListenersToSocialIconButton();
+function onAddTodo() {
+    let priorityIdValue = priorityIdEl.value;
+    let todoUserInputValue = todoUserInputEl.value;
 
-const skillIcon = document.getElementsByClassName("tooltip");
 
-
-const addEventListenersToSkillIcon = () => {
-    for (let i = 0; i < skillIcon.length; i++) {
-        skillIcon[i].addEventListener("click", (e) => {
-            console.log(e.target)
-            updateDescription(e.target.id);
-        });
+    if (todoUserInputValue === "") {
+        alert("Enter Valid Text");
+        return;
+    } else {
+        let userClicked = confirm("Do you want to Add in TodoList");
+        if (userClicked) {
+            todosCount = todosCount + 1;
+            let newTodo = {
+                text: todoUserInputValue,
+                uniqueNo: todosCount,
+                isChecked: false,
+                priority: priorityIdValue
+            };
+            todoList.push(newTodo);
+            createAndAppendTodo(newTodo);
+            todoUserInputEl.value = "";
+        } else {
+            todoUserInputEl.value = "";
+        }
     }
+
+    updateTasksCount();
+}
+addTodoButtonEl.onclick = function() {
+    onAddTodo();
 };
 
-addEventListenersToSkillIcon();
+function onTodoStatusChange(checkboxId, labelId, todoId) {
+    let checkboxEl = document.getElementById(checkboxId);
+    let labelEl = document.getElementById(labelId);
+    labelEl.classList.toggle("checked");
 
-const descriptionDiv = document.getElementsByClassName("about-section");
-const updateDescription = (id) => {
-    let description = "";
+    let todoObjectIndex = todoList.findIndex(function(eachTodo) {
+        let eachTodoId = "todo" + eachTodo.uniqueNo;
 
-    switch (id) {
-        case "googleColab":
-            description = "Google Colab: User-friendly platform for code writing, execution, and sharing. Beloved by AI experts, students, developers, and researchers for data analysis, ML, and AI exploration.";
+        if (eachTodoId === todoId) {
+            return true;
+        } else {
+            return false;
+        }
+    });
+
+    let todoObject = todoList[todoObjectIndex];
+
+    if (todoObject.isChecked === true) {
+        todoObject.isChecked = false;
+
+    } else {
+        todoObject.isChecked = true;
+    }
+    updateTasksCount();
+}
+
+function onDeleteTodo(todoId) {
+    let todoEl = document.getElementById(todoId);
+    todoItemsContainerEl.removeChild(todoEl);
+
+    let deleteElIndex = todoList.findIndex(function(eachTodo) {
+        let eachTodoId = "todo" + eachTodo.uniqueNo;
+        if (eachTodoId === todoId) {
+            return true;
+        } else {
+            return false;
+        }
+    });
+    todoList.splice(deleteElIndex, 1);
+    updateTasksCount();
+}
+
+function createAndAppendTodo(todo) {
+
+    let todoId = "todo" + todo.uniqueNo;
+    let checkboxId = "checkbox" + todo.uniqueNo;
+    let labelId = "label" + todo.uniqueNo;
+
+    let todoEl = document.createElement("li");
+    todoEl.classList.add("todo-item-container", "d-flex", "flex-row");
+    todoEl.id = todoId;
+    todoItemsContainerEl.appendChild(todoEl);
+
+    let inputEl = document.createElement("input");
+    inputEl.type = "checkbox";
+    inputEl.id = checkboxId;
+    inputEl.checked = todo.isChecked;
+    inputEl.onclick = function() {
+        onTodoStatusChange(checkboxId, labelId, todoId);
+    }
+    inputEl.classList.add("checkbox-input");
+    todoEl.appendChild(inputEl);
+
+    let labelContainer = document.createElement("div");
+    labelContainer.classList.add("label-container", "d-flex", "flex-row");
+    // update label-container when the p1 the label container border color should be red,
+    // when p2 its yellow , when p3 its blue, when p4 its white
+    todoEl.appendChild(labelContainer);
+
+    let labelEl = document.createElement("label");
+    labelEl.setAttribute("for", checkboxId);
+    labelEl.id = labelId;
+    labelEl.classList.add("checkbox-label");
+    labelEl.textContent = todo.text;
+    if (todo.isChecked === true) {
+        labelEl.classList.add("checked");
+    }
+    labelContainer.appendChild(labelEl);
+
+    let priorityEl = document.createElement("span");
+    priorityEl.classList.add("priority");
+    priorityEl.textContent = todo.priority;
+    labelContainer.appendChild(priorityEl);
+
+
+
+    let deleteIconContainer = document.createElement("div");
+    deleteIconContainer.classList.add("delete-icon-container");
+    labelContainer.appendChild(deleteIconContainer);
+
+    let deleteIcon = document.createElement("i");
+    deleteIcon.classList.add("far", "fa-trash-alt", "delete-icon");
+    deleteIcon.onclick = function() {
+        onDeleteTodo(todoId);
+    }
+    deleteIconContainer.appendChild(deleteIcon);
+
+    switch (todo.priority) {
+        case "P1":
+            labelContainer.style.borderColor = "red";
+            priorityEl.style.backgroundColor = "red"
+            priorityEl.style.color = "white";
             break;
-        case "gradio":
-            description = "Gradio: A user-friendly tool that allows to create and share interactive Al Apps without extensive Coding Knowledge.";
+        case "P2":
+            labelContainer.style.borderColor = "yellow";
+            priorityEl.style.backgroundColor = "yellow";
+            priorityEl.style.color = "black";
             break;
-        case "openAi":
-            description = "OpenAI: An AI-based chat service powered by Open AI's language model. OpenAI APIs offer access to advanced language models and AI capabilities. Integrate NLP, text generation, and more into your apps to revolutionize communication and problem-solving. ";
-            break;
-        case "playHt":
-            description = "PlayHT: PlayHT is a platform that allows you to clone voices using artificial intelligence. The platform uses a deep learning model to train a voice clone that sounds almost exactly like the original voice.";
-            break;
-        case "huggingFace":
-            description = "HuggingFace: The ultimate destination for building, training, and deploying cutting-edge machine learning models! Revolutionize your AI projects with state-of-the-art NLP and more!";
-            break;
-        case "langChain":
-            description = "LangChain: Seamlessly combine Large Language Models (LLMs) with external computation/data. Build chatbots, analyze data effortlessly. Open source for contributions.";
+        case "P3":
+            labelContainer.style.borderColor = "blue";
+            priorityEl.style.backgroundColor = "blue";
+            priorityEl.style.color = "white";
             break;
         default:
-            description = "Description of the selected icon will appear here.";
+            labelContainer.style.borderColor = "white";
     }
-    for (let i = 0; i < descriptionDiv.length; i++) {
-        descriptionDiv[i].textContent = description;
-    }
-};
 
-updateDescription("googleColab");
+    updateTasksCount()
+}
+
+for (let todo of todoList) {
+    createAndAppendTodo(todo);
+}
+
+saveTodoButtonEl.addEventListener("click", function() {
+    alert("Save Successfully!");
+})
